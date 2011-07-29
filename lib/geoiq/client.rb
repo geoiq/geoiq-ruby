03ec2e5f76bc08f1bb4ac6e72ebe85d0db9af038
Tim Waters <chippy2005@gmail.com>
2011-07-29 15:45:26 +0100

    add longer timeout 240 seconds as default

--- lib/geoiq/client.rb
+++ lib/geoiq/client.rb
@@ -2,8 +2,9 @@ module Geoiq
 
   class Client
     include HTTParty
-    #base_uri "geocommons.com"
-     def initialize(url="localhost.local", username=nil, password=nil)
+    default_timeout 240
+   
+     def initialize(url, username=nil, password=nil)
       @username = username
       @url = url
       self.class.base_uri(url)
